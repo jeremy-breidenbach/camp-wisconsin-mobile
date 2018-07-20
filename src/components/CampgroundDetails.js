abc7e0c17ddbf161da014b9f179ce908c6641053
Jeremy Breidenbach <jeremy.breidenbach@gmail.com>
2018-07-19 22:46:09 -0500

    Added Amenities SectionList to CampgroundDetails page

--- src/components/CampgroundDetails.js
+++ src/components/CampgroundDetails.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { ScrollView, View, StyleSheet } from 'react-native';
-import { List, ListItem, Text, Button, Card } from 'react-native-elements';
+import { ScrollView, StyleSheet, View, SectionList } from 'react-native';
+import { Button, Card, List, ListItem, Text } from 'react-native-elements';
 
 class CampgroundDetails extends Component {
     static navigationOptions = ({ navigation }) => {
@@ -13,18 +13,54 @@ class CampgroundDetails extends Component {
         super(props);
         this.state = {
             isLoading: false,
-            amenitiesWithinFacility: []
+            amenitiesListSections: []
         };
     }
 
     componentDidMount() {
         this.parseAmenities(this.props.navigation.getParam('campgroundAmenities'));
+        console.log(this.state.amenitiesWithinFacility);
     }
 
     parseAmenities(amenitiesJsonString) {
         const amenitiesArray = JSON.parse(amenitiesJsonString);
+        const amenitiesWithinFacility = [];
+        const amenitiesWithin10Miles = [];
+        const amenitiesGreaterThan10Miles = [];
+        const amenitiesGreaterThan1Mile = [];
+        const amenitiesListSections = [];
+
+        for (let i = 0; i < amenitiesArray.length; i++) {
+            if (amenitiesArray[i].distance === 'Within Facility') {
+                amenitiesWithinFacility.push(amenitiesArray[i].name);
+            } else if (amenitiesArray[i].distance === 'Within 10 Miles') {
+                amenitiesWithin10Miles.push(amenitiesArray[i].name);
+            } else if (amenitiesArray[i].distance === 'Greater Than 10 Miles') {
+                amenitiesGreaterThan10Miles.push(amenitiesArray[i].name);
+            } else if (amenitiesArray[i].distance === 'Greater Than 1 Mile') {
+                amenitiesGreaterThan1Mile.push(amenitiesArray[i].name);
+            }
+        }
+        
+        if (amenitiesWithinFacility.length > 0) {
+            amenitiesListSections.push({ title: 'Within Facility', data: amenitiesWithinFacility });
+        }
+
+        if (amenitiesGreaterThan1Mile.length > 0) {
+            amenitiesListSections.push({ title: 'Greater Than 1 Mile', data: amenitiesGreaterThan1Mile });
+        }
+
+        if (amenitiesWithin10Miles.length > 0) {
+            amenitiesListSections.push({ title: 'Within 10 Miles', data: amenitiesWithin10Miles });
+        }
+
+        if (amenitiesGreaterThan10Miles.length > 0) {
+            amenitiesListSections.push({ title: 'Greater Than 10 Miles', data: amenitiesGreaterThan10Miles });
+        }
+
+
         this.setState({
-            amenitiesWithinFacility: amenitiesArray.filter(amenity => amenity.distance === 'Within Facility')
+            amenitiesListSections
         });
     }
 
@@ -52,16 +88,12 @@ class CampgroundDetails extends Component {
                     </View>
                     <View>
                         <Card title='Amenities'>
-                            <List>
-                                {
-                                    this.state.amenitiesWithinFacility.map((l, i) => (
-                                        <ListItem 
-                                            key={i}
-                                            title={l.name}
-                                        />
-                                    ))
-                                }
-                            </List>
+                            <SectionList 
+                                renderItem={({item, index, section}) => <ListItem key={index} title={item} hideChevron />}
+                                renderSectionHeader={({ section: { title } }) => (<Text style={{ fontWeight: 'bold' }}>{title}</Text>)}
+                                sections={this.state.amenitiesListSections}
+                                keyExtractor={(item, index) => index}
+                            />
                         </Card>
                     </View>
                 </ScrollView>
